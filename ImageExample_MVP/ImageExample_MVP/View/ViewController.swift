fcc2853d6829809815cbf5b60f53080d9d031b49
YoonAh-dev <tlsdbsdk05250@gmail.com>
2023-04-11 16:35:37 +0900

    [ADD] MVP 패턴 예제 프로젝트 추가

---
+++ ImageExample_MVP/ImageExample_MVP/View/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  ImageExample_MVP
+//
+//  Created by SHIN YOON AH on 2023/04/11.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    // MARK: - ui component
+
+    @IBOutlet weak var photoCollectionView: UICollectionView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var photoCountLabel: UILabel!
+
+    // MARK: - life cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - IBAction
+
+    @IBAction func didTapLeftButton(_ sender: Any) {
+
+    }
+
+    @IBAction func didTapRightButton(_ sender: Any) {
+
+    }
+
+    @IBAction func didTapSubmitButton(_ sender: Any) {
+
+    }
+}
