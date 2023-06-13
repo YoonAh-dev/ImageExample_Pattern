68545b41df1ce1af93c5578b659771957cab9626
YoonAh-dev <tlsdbsdk05250@gmail.com>
2023-06-13 14:38:23 +0900

    feat: increase or decrease count with viewmodel

--- ImageExample_MVVM_RxSwift/ImageExample_MVVM_RxSwift/View/ViewController.swift
+++ ImageExample_MVVM_RxSwift/ImageExample_MVVM_RxSwift/View/ViewController.swift
@@ -7,6 +7,9 @@
 
 import UIKit
 
+import RxCocoa
+import RxSwift
+
 final class ViewController: UIViewController {
 
     // MARK: - ui component
@@ -14,78 +17,82 @@ final class ViewController: UIViewController {
     @IBOutlet weak var photoCollectionView: UICollectionView!
     @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var photoCountLabel: UILabel!
+    @IBOutlet weak var leftButton: UIButton!
+    @IBOutlet weak var rightButton: UIButton!
+    @IBOutlet weak var submitButton: UIButton!
 
     // MARK: - property
 
-    private var imageURLs: [String] = [] {
-        didSet { self.photoCollectionView.reloadData() }
-    }
+    private let disposeBag = DisposeBag()
+    private let viewModel: ViewModel = ViewModel()
 
     // MARK: - life cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.configureDelegation()
+        self.bindInput()
+        self.bindOutput()
     }
 
     // MARK: - func
 
-    private func configureDelegation() {
-        self.photoCollectionView.delegate = self
-        self.photoCollectionView.dataSource = self
+    private func bindInput() {
+        self.leftButton.rx.tap
+            .withUnretained(self)
+            .bind { _ in self.viewModel.handleCount(with: .left) }
+            .disposed(by: self.disposeBag)
+
+        self.rightButton.rx.tap
+            .withUnretained(self)
+            .bind { _ in self.viewModel.handleCount(with: .right) }
+            .disposed(by: self.disposeBag)
     }
 
-    // MARK: - IBAction
-
-    @IBAction func didTapLeftButton(_ sender: Any) {
-
+    private func bindOutput() {
+        self.viewModel.countRelay
+            .map { "\($0)" }
+            .bind(to: self.photoCountLabel.rx.text)
+            .disposed(by: disposeBag)
     }
 
-    @IBAction func didTapRightButton(_ sender: Any) {
-
-    }
-
-    @IBAction func didTapSubmitButton(_ sender: Any) {
-
-    }
 }
 
-// MARK: - UICollectionViewDataSource
-extension ViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.imageURLs.count
-    }
-
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCollectionViewCell.identifier, for: indexPath) as? PhotoCollectionViewCell else { return UICollectionViewCell() }
-        cell.configureCell(imageURL: self.imageURLs[indexPath.item])
-        return cell
-    }
-}
-
-// MARK: - UICollectionViewDelegate
-extension ViewController: UICollectionViewDelegate {
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let width = scrollView.frame.width
-        let offset = scrollView.contentOffset.x
-    }
-}
-
-// MARK: - UICollectionViewDelegateFlowLayout
-extension ViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.width * 0.53)
-    }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
-    }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
-    }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets.zero
-    }
-}
+//// MARK: - UICollectionViewDataSource
+//extension ViewController: UICollectionViewDataSource {
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return self.imageURLs.count
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCollectionViewCell.identifier, for: indexPath) as? PhotoCollectionViewCell else { return UICollectionViewCell() }
+//        cell.configureCell(imageURL: self.imageURLs[indexPath.item])
+//        return cell
+//    }
+//}
+//
+//// MARK: - UICollectionViewDelegate
+//extension ViewController: UICollectionViewDelegate {
+//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+//        let width = scrollView.frame.width
+//        let offset = scrollView.contentOffset.x
+//    }
+//}
+//
+//// MARK: - UICollectionViewDelegateFlowLayout
+//extension ViewController: UICollectionViewDelegateFlowLayout {
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        return CGSize(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.width * 0.53)
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//        return 0
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+//        return 0
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+//        return UIEdgeInsets.zero
+//    }
+//}
--- ImageExample_MVVM_RxSwift/ImageExample_MVVM_RxSwift/ViewModel/ViewModel.swift
+++ ImageExample_MVVM_RxSwift/ImageExample_MVVM_RxSwift/ViewModel/ViewModel.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+
+import RxCocoa
+import RxSwift
+
+final class ViewModel {
+
+    enum Direction {
+        case left, right
+    }
+
+    // MARK: - property
+
+    let countRelay: BehaviorRelay<Int> = BehaviorRelay(value: 0)
+
+    // MARK: - func
+    
+    func handleCount(with direction: Direction) {
+        let count = self.calculateCount(with: direction)
+        self.countRelay.accept(count)
+    }
+
+    func fetchImage() {
+        
+    }
+
+    // MARK: - Private - func
+
+    private func calculateCount(with direction: Direction) -> Int {
+        var currentValue = self.countRelay.value
+
+        switch direction {
+        case .left:
+            guard currentValue > 0 else { return 0 }
+            currentValue -= 1
+        case .right:
+            currentValue += 1
+        }
+
+        return currentValue
+    }
+}
