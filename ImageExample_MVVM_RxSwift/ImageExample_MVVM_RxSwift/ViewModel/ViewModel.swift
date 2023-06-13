ad034efa7197f35f1752ec62d72dafdfa738b7ee
YoonAh-dev <tlsdbsdk05250@gmail.com>
2023-06-13 19:22:21 +0900

    chore: delete rxswift mark annotation

--- ImageExample_MVVM_RxSwift/ImageExample_MVVM_RxSwift/ViewModel/ViewModel.swift
+++ ImageExample_MVVM_RxSwift/ImageExample_MVVM_RxSwift/ViewModel/ViewModel.swift
@@ -7,6 +7,9 @@
 
 import Foundation
 
+import RxCocoa
+import RxSwift
+
 final class ViewModel {
 
     enum Direction {
@@ -17,9 +20,9 @@ final class ViewModel {
 
     private let service: UnsplashService
 
-//    let countRelay: BehaviorRelay<Int> = BehaviorRelay(value: 1)
-//    let imageUrlRelay: BehaviorRelay<[String]> = BehaviorRelay(value: [])
-//    let currentPageRelay: PublishRelay<Int> = PublishRelay()
+    let countRelay: BehaviorRelay<Int> = BehaviorRelay(value: 1)
+    let imageUrlRelay: BehaviorRelay<[String]> = BehaviorRelay(value: [])
+    let currentPageRelay: PublishRelay<Int> = PublishRelay()
 
     // MARK: - init
 
@@ -31,35 +34,35 @@ final class ViewModel {
     
     func handleCount(with direction: Direction) {
         let count = self.calculateCount(with: direction)
-//        self.countRelay.accept(count)
+        self.countRelay.accept(count)
     }
 
     func fetchImage() {
         Task {
-//            let count = self.countRelay.value
-//            let urls = await self.service.imageURLs(count: count)
-//            self.imageUrlRelay.accept(urls)
+            let count = self.countRelay.value
+            let urls = await self.service.imageURLs(count: count)
+            self.imageUrlRelay.accept(urls)
         }
     }
 
     func handleCurrentPage(with width: Double, _ offset: Double) {
         let currentPage = Int(offset / width)
-//        self.currentPageRelay.accept(currentPage)
+        self.currentPageRelay.accept(currentPage)
     }
 
     // MARK: - Private - func
 
     private func calculateCount(with direction: Direction) -> Int {
-//        var currentValue = self.countRelay.value
-//
-//        switch direction {
-//        case .left:
-//            guard currentValue > 1 else { return 1 }
-//            currentValue -= 1
-//        case .right:
-//            currentValue += 1
-//        }
-//
-//        return currentValue
+        var currentValue = self.countRelay.value
+
+        switch direction {
+        case .left:
+            guard currentValue > 1 else { return 1 }
+            currentValue -= 1
+        case .right:
+            currentValue += 1
+        }
+
+        return currentValue
     }
 }
